03b2c904f249f0023030dbad60f26a652d141f3f
dshxh-23 <dhyeyshah2336@gmail.com>
2026-04-09 20:14:39 +0530

    feat: create blueprint of linked stack

---
+++ data_structures/stack/linked_stack.py
@@ -0,0 +1,42 @@
+class LinkedStack:
+    def ___init___(self):
+        self.top = None
+        self._size = 0
+    
+    def __repr__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __bool__(self):
+        pass
+
+    def __iter__(self):
+        pass
+
+
+    ###################
+
+
+    def is_empty():
+        ...
+
+    
+    def push(self, val):
+        ...
+
+    
+    def pop(self):
+        ...
+
+    
+    def peek(self):
+        ...
+
+    
+    def size(self):
+        ...
