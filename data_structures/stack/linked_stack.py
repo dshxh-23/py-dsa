d6c5613128e998d85f56239d79760b2eb5cfbd4f
dshxh-23 <dhyeyshah2336@gmail.com>
2026-04-10 16:43:11 +0530

    feat: implement core functions of linked stack

--- data_structures/stack/linked_stack.py
+++ data_structures/stack/linked_stack.py
@@ -1,3 +1,5 @@
+from core.nodes import Node
+
 class LinkedStack:
     def ___init___(self):
         self.top = None
@@ -13,7 +15,7 @@ class LinkedStack:
         pass
 
     def __bool__(self):
-        pass
+        return self.top is not None
 
     def __iter__(self):
         pass
@@ -22,21 +24,42 @@ class LinkedStack:
     ###################
 
 
-    def is_empty():
-        ...
+    def is_empty(self):
+        return self.top is None
 
     
     def push(self, val):
-        ...
+        new = Node(val)
+        new.next = self.top
+        self.top = new
+        self._size += 1
 
-    
+
+    def _empty(self):
+        if self._size != 1:
+            raise ...
+        del_val = self.top.data        
+        self.top = None
+        self._size = 0
+        return del_val
+        
     def pop(self):
-        ...
-
+        if self.top is None:
+            raise ...
+        
+        if self._size == 1:
+            return self._empty()
+        
+        pop_val = self.top.data
+        self.top = self.top.next
+        return pop_val
+        
     
     def peek(self):
-        ...
+        if self.top is None:
+            raise ...
+        return self.top.data
 
     
     def size(self):
-        ...
+        return self._size
