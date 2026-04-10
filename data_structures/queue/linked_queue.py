f25300eb78fab3e7ebbbda1b56f10ad9e0ac0899
dshxh-23 <dhyeyshah2336@gmail.com>
2026-04-10 23:47:49 +0530

    feat: define basic blueprint for linked_queue

---
+++ data_structures/queue/linked_queue.py
@@ -0,0 +1,38 @@
+from core.nodes import Node
+# import queue exceptions
+
+class LinkedQueue():
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __iter__(self):
+        pass
+
+    def __bool__(self):
+        pass
+
+    def __str__(self):
+        pass
+    
+
+
+    def enqueue(self, val):
+        pass
+
+    def dequeue(self):
+        pass
+
+    def peek(self):
+        pass
+
+    def is_empty(self):
+        pass
+
+    def size(self):
+        pass
