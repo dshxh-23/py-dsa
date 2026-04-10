0126fa225609af002646617ef7501bcec996a42a
dshxh-23 <dhyeyshah2336@gmail.com>
2026-04-10 20:06:22 +0530

    fix: used ___init___ instead of __init__ in LinkedStack

--- data_structures/stack/linked_stack.py
+++ data_structures/stack/linked_stack.py
@@ -1,7 +1,7 @@
 from core.nodes import Node
 
 class LinkedStack:
-    def ___init___(self):
+    def __init__(self):
         self.top = None
         self._size = 0
     
@@ -9,16 +9,20 @@ class LinkedStack:
         pass
 
     def __str__(self):
-        pass
+        stack = [str(element) for element in self]
+        return " | ".join(stack)
 
     def __len__(self):
-        pass
+        return self._size
 
     def __bool__(self):
         return self.top is not None
 
     def __iter__(self):
-        pass
+        curr = self.top
+        while curr:
+            yield curr.data
+            curr = curr.next
 
 
     ###################
@@ -52,6 +56,7 @@ class LinkedStack:
         
         pop_val = self.top.data
         self.top = self.top.next
+        self._size -= 1
         return pop_val
         
     
@@ -63,3 +68,25 @@ class LinkedStack:
     
     def size(self):
         return self._size
+    
+
+def main():
+    ls1 = LinkedStack()
+    ls1.push(10)
+    ls1.push(20)
+
+    print(ls1)          # 20 | 10
+    print(ls1.pop())    # 20
+    print(ls1)          # 10
+    print(ls1.pop())    # 10
+
+    # print(ls1.pop())
+    # print(ls1.peek())
+
+    ls1.push(30)
+    print(ls1.peek())
+    ls1.push(40)
+    print(ls1.peek())
+
+if __name__ == "__main__":
+    main()
