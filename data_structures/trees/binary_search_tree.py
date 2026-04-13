6059688537130505067be6345d9c274e67d1e2df
dshxh-23 <dhyeyshah2336@gmail.com>
2026-04-14 00:21:51 +0530

    feat: create BST implementation blueprint

---
+++ data_structures/trees/binary_search_tree.py
@@ -0,0 +1,75 @@
+from core.nodes import TreeNode
+from core.exceptions import *       # Replace * with specific errors used
+
+# ======================================== #
+
+class BinarySearchTree():
+
+    def __init__():
+        pass
+
+    
+    # ---------- DUNDER METHODS ----------
+
+    def __repr__():
+        pass
+
+    def __bool__():
+        pass
+
+    def __iter__():
+        pass
+
+    def __len__():
+        pass
+
+    def __str__():
+        pass
+
+    # ---------- CORE UTILS ----------
+
+    def is_empty():
+        pass
+
+
+    def size():
+        pass
+
+
+    # ---------- BASIC OPERAITONS ----------
+
+    def insert():
+        pass
+
+
+    def search():
+        pass
+
+
+    def delete():
+        pass
+
+
+    # ---------- UTILITIES ----------
+
+    def find_min():
+        pass
+
+
+    def find_max():
+        pass
+
+
+    def height():
+        pass
+
+
+# ======================================== #
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
