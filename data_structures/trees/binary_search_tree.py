affcd1d0cc3f8248d03de4dd64861469078dd0f3
dshxh-23 <dhyeyshah2336@gmail.com>
2026-04-16 22:35:30 +0530

    feat: implement insert in bst

--- data_structures/trees/binary_search_tree.py
+++ data_structures/trees/binary_search_tree.py
@@ -1,52 +1,96 @@
 from core.nodes import TreeNode
 from core.exceptions import *       # Replace * with specific errors used
 
+from data_structures.queue.linked_queue import LinkedQueue
 # ======================================== #
 
 class BinarySearchTree():
 
-    def __init__():
-        pass
+    def __init__(self):
+        self.root = None
+        self._size = 0
 
     
     # ---------- DUNDER METHODS ----------
 
-    def __repr__():
+    def __repr__(self):
         pass
 
-    def __bool__():
+    def __bool__(self):
         pass
 
-    def __iter__():
+    def __iter__(self):
         pass
 
-    def __len__():
+    def __len__(self):
         pass
 
-    def __str__():
+    def __str__(self):
         pass
 
     # ---------- CORE UTILS ----------
 
-    def is_empty():
-        pass
+    def is_empty(self):
+        return True if self.root is None else False
 
 
-    def size():
-        pass
+    def size(self):
+        return self._size
 
 
     # ---------- BASIC OPERAITONS ----------
 
-    def insert():
-        pass
+    def insert(self, val):
+        new_node = TreeNode(val)
 
+        if self.root is None:
+            self.root = new_node
+            self._size += 1
+            return self.root
+        
+        parent = None
+        curr = self.root
+
+        while curr:
+            parent = curr
+            if new_node.data < curr.data:
+                curr = curr.left
+            
+            elif new_node.data > curr.data:
+                curr = curr.right
+
+            else:
+                raise DuplicateKeyError()
+        
+        if parent.data > new_node.data:
+            parent.left = new_node
+
+        elif parent.data < new_node.data:
+            parent.right = new_node
+
+        self._size += 1
+        return self.root
+        
 
     def search():
         pass
 
 
     def delete():
+        pass
+
+
+    # ---------- TRAVERSALS  ----------
+
+    def inorder(self):
+        pass
+
+    
+    def preorder(self):
+        pass
+
+
+    def postorder(self):
         pass
 
 
@@ -68,7 +112,15 @@ class BinarySearchTree():
 
 
 def main():
-    pass
+    bst = BinarySearchTree()
+    bst.insert(10)
+    bst.insert(12)
+    bst.insert(38)
+    bst.insert(3)
+    bst.insert(20)
+    bst.insert(8)
+    bst.insert(2)
+    print(bst)
 
 
 if __name__ == "__main__":
