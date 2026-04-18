b49c6ede6097f4c43ee9a1b7686f7ca9b2d840a3
dshxh-23 <dhyeyshah2336@gmail.com>
2026-04-18 21:28:39 +0530

    feat: complete all methods implementation

--- data_structures/arrays/static_array.py
+++ data_structures/arrays/static_array.py
@@ -1,29 +1,73 @@
+from core.exceptions import ArrayOverflowError, ArrayUnderflowError, ArrayIndexOutOfBoundsError
 class StaticArray:
     def __init__(self, cap):
         self._capacity = cap
         self._data = [None] * cap
         self._size = 0
 
-    def insert(self, index, val):
-        ...
 
-    def append(self, val):
-        ...
-
-    def delete(self, index):
-        ...
-
-    def get(self, index):
-        ...
-
-    def set(self, index):
-        ...
+    # ---------- BASIC ----------
 
     def size(self):
-        ...
+        return self._size
 
     def capacity(self):
-        ...
+        return self._capacity
 
     def is_full(self):
-        ...
+        return self._size == self._capacity
+    
+    def is_empty(self):
+        return self._size == 0
+
+
+    # ---------- ACCESS ----------
+
+    def get(self, index):
+        if index < 0 or index >= self._size:
+            raise ArrayIndexOutOfBoundsError()
+        return self._data[index]
+
+
+    def set(self, index, val):
+        if index < 0 or index >= self._size:
+            raise ArrayIndexOutOfBoundsError()
+        self._data[index] = val
+        
+
+    # ---------- OPERATIONS ----------
+
+    def insert(self, index, val):
+        if index < 0 or index >= self._size:
+            raise ArrayIndexOutOfBoundsError()
+        
+        if self.is_full():
+            raise ArrayOverflowError()
+        
+        for i in range(self._size, index, -1):
+            self._data[i] = self._data[i-1]
+        
+        self._data[index] = val
+        self._size += 1
+
+
+    def append(self, val):
+        if self.is_full():
+            raise ArrayOverflowError()
+        
+        self._data[self._size] = val
+        self._size += 1
+
+
+    def delete(self, index):
+        if index < 0 or index >= self._size:
+            raise ArrayIndexOutOfBoundsError()
+
+        if self.is_empty():
+            raise ArrayUnderflowError()
+        
+        for i in range(index, self._size-1):
+            self._data[i] = self._data[i+1]
+        self._data[self._size-1] = None
+
+        self._size -= 1
