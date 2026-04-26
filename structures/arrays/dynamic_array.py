0850e08de3195363a1f6902b699374a944d940c5
dshxh-23 <dhyeyshah2336@gmail.com>
2026-04-26 11:23:25 +0530

    feat: impl dynamic array operations

--- structures/arrays/dynamic_array.py
+++ structures/arrays/dynamic_array.py
@@ -47,24 +47,63 @@ class DynamicArray:
     
     # ---------- OPERATIONS ----------
     
-    def append():
-        pass
+    def append(self, val):
+        """insert element at end of array"""
+
+        if self._size == self.capacity:
+            self._resize(self._capacity * 2)
+        
+        self._data[self._size] = val
+        self._size += 1
 
     # ----------
 
-    def insert():
-        pass
+    def insert(self, index, val):
+        """insert an element at a particular index"""
+
+        if index < 0 or index > self._size:
+            raise ArrayIndexOutOfBoundsError()
+        
+        # resize dynamic array if capacity full
+        if self._size == self._capacity:
+            self._resize(self._capacity * 2)
+
+        # shift right
+        for i in range(self._size, index, -1):
+            self._data[i] = self._data[i-1]
+
+        self._data[index] = val
+        self._size += 1 
 
     # ----------
 
-    def delete():
-        pass
+    def delete(self, index):
+        """deletes a element at a specific index"""
+
+        if self.is_empty():
+            raise ArrayUnderflowError()       
+
+        if index < 0 or index >= self._size:
+            raise ArrayIndexOutOfBoundsError()
+        
+        value = self._data[index]
+        
+        for i in range(index, self._size):
+            self._data[i] = self._data[i+1]
+        
+        self._size -= 1
+        return value
 
 
-    # ---------- HELPER ----------
+    # ---------- INTERNAL ----------
 
-    def _resize(self):
-        pass
+    def _resize(self, new_cap):
+        new_data = [None] * new_cap
+        for i in range(self._size):
+            new_data[i] = self._data[i]
+
+        self._data = new_data
+        self._capacity = new_cap
 
     # ---------- DUNDER METHODS ----------
 
