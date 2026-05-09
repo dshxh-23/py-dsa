9a1e8e28a5848976d2fa64e868a2e2b6048f1c40
dshxh-23 <dhyeyshah2336@gmail.com>
2026-05-09 18:55:15 +0530

    feat: impl bubble sort

---
+++ algorithms/sorting/bubble_sort/sort.py
@@ -0,0 +1,14 @@
+from algorithms.utils.swap import swap
+def sort(arr, reverse=False):
+    """in-place sorting using bubble sort"""
+
+    for i in range(len(arr)):
+        for j in range(0, len(arr)-i-1):
+            
+            if reverse:
+                if arr[j] < arr[j+1]:
+                    swap(arr, j, j+1)
+            else:
+                if arr[j] > arr[j+1]:
+                    swap(arr, j, j+1)
+    return
