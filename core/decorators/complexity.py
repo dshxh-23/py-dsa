668c85199536740f4cf8b93f2a328541671a71ab
dshxh-23 <dhyeyshah2336@gmail.com>
2026-05-27 18:14:34 +0530

    feat: impl complexity_report utility method

--- core/decorators/complexity.py
+++ core/decorators/complexity.py
@@ -28,3 +28,41 @@ def complexity(time, space, note=None):
 
         return wrapper
     return decorator
+
+
+def complexity_report(obj):
+
+    if isinstance(obj, type):   # checks if obj is a class, use it
+        cls = obj
+    else:                       # if object is an instance, get its class
+        cls = type(obj)
+
+    title = f"Complexity Report: {cls.__name__}"
+    print(f"\n{title}")
+    print("="*len(title))
+
+    found = False
+
+    for name in dir(cls):
+        
+        # omit dunder methods
+        if name.startswith("__") and name.endswith("__"):
+            continue
+
+        method = getattr(cls, name, None)   # get attribute or None if it doesn't exist
+
+        if callable(method) and hasattr(method, '_complexity'):
+            info = method._complexity
+
+            print(f"\n  {name}()")
+            print(f"\tTime\t:\t{info.time}")
+            print(f"\tSpace\t:\t{info.time}")
+            if info.note:
+                print(f"\tNote\t:\t{info.note}")
+            
+            found = True
+        
+        if not found:
+            print("No complexity decorators found.")
+        
+        print()
